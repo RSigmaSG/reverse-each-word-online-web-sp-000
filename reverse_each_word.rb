eedcda13a914bfd56f4f420ce41820be8b09ca4b
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-16 02:42:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(input)
   
-  output_arr = []
+  output_str = ""
   
   input_arr = input.split(" ")
   
-  input_arr.collect do
+  input_arr.collect do |word|
     
-    
+    output_str << 
     
   end
   
