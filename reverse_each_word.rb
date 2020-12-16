304801a55c35bb30c8dec48f993065ebb0135ae6
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-16 02:56:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(input)
   
   input_arr = input.split(" ")
   
-  input_arr.collect do {|word|word.reverse}.join(" ")
+  input_arr.collect do {|word| word.reverse}.join(" ")
   
   return input_arr
   
